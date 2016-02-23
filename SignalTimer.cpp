162cfb918e3c406e7a089b6e711eafb2c5895d08
LikithGowdaHS <hslg.109@gmail.com>
2016-02-23 21:45:12 +0530

    Update SignalTimer.cpp

--- SignalTimer.cpp
+++ SignalTimer.cpp
@@ -26,24 +26,3 @@ class SignalTimer {
 			return seconds >= elapsedTime();
 		}
 };
-
-
-/*int main() {
-	unsigned long seconds = 10;
-	timer t;
-	t.start();
-	cout << "timer started . . ." << endl;
-	while(true) {
-		if(t.elapsedTime() >= seconds) {
-			break;
-		}
-		else {
-			// do other things
-		}
-	}
-	cout << seconds <<  " seconds elapsed" << endl;
-
-	cin >> seconds;	// it's just to stop the execution and look at the output
-	
-	return 0;
-}*/
